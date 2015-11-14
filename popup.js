e6700bcebbf1ddecf2957b2b3063f660a2357844
kburk1997 <kburk97@optonline.net>
2015-11-14 16:08:42 -0500

    Update popup.js
    Add some test javascript code

--- popup.js
+++ popup.js
@@ -1,14 +1,6 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-
+//Main function
 document.addEventListener('DOMContentLoaded', function() {
-  
+  //Console log the current url for test purposes
+  console.log(document.URL);
 });
