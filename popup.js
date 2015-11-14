20a2343de7dcee0873f4cfc643d196d25934f563
kburk1997 <kburk97@optonline.net>
2015-11-14 17:08:38 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,81 @@
+//Load JSON
+function loadJSON(callback) {
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', 'data.json', true);
+	xobj.onreadystatechange = function () {
+	if (xobj.readyState == 4 && xobj.status == "200") {
 
+	// .open will NOT return a value but simply returns undefined in async mode so use a callback
+	callback(xobj.responseText);
+
+	}
+	}
+	xobj.send(null);
+
+	}
+
+
+
+
+//Get current url of tab
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+}
+//RenderStatus
+//Changes the status of the popup
+function renderStatus(statusText) {
+ document.getElementById('status').textContent = statusText;
+}
 //Main function
 document.addEventListener('DOMContentLoaded', function() {
-  //Store the document url and display for test purposes
-  var url = document.URL;
-  console.log(url);
+	//get url of current tab
+  /*getCurrentTabUrl(function(url) {
+     // Display URL
+     renderStatus(url);
+  });*/
+
+  //Load data
+	// Call to function with anonymous callback
+	loadJSON(function(response) {
+		alert("hello world");
+		// Do Something with the response e.g.
+		jsonresponse = JSON.parse(response);
+
+		// Assuming json data is wrapped in square brackets as Drew suggests
+		//Loop through the entire json
+		for(i = 0; i<31; ++i){
+			alert(jsonresponse[i].data.content)
+		}
+		renderStatus(jsonresponse[0].data.content);
+
+	});
 });
